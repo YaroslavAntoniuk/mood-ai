dc34b38a87c9dd5d223ccb9e828879bf4dbbffa9
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-11 14:55:49 +0200

    Updated home page redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+import { auth, currentUser } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+
+  const href = userId ? "/journal" : "/new-user";
+
   return <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
     <div className="w-full max-w-[600px] mx-auto">
       <h1 className="text-6xl">Journal Mood App AI</h1>
       <p className="text-2xl text-white/50 p-4">The app for tracking your mood with the help of AI. Make records every day, track your mood, change your life.</p>
 
       <div className="w-full flex justify-center items-center">
-        <Link href="/journal">
+        <Link href={href}>
           <button className="bg-black px-6 py-4 rounded-lg text-xl shadow-[0_0_40px_1px_#48abe0]">Get Started</button>
         </Link>
       </div>
