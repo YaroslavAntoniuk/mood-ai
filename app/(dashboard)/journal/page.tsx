5e766f12e29774ca2fd6544b59205fb1160e5849
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-11 19:35:50 +0200

    Added logic for fetching journal entries

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,37 @@
-const Journal = () => {
-  return <div>Journal</div>
+import JournalEntryCard from "@/components/JournalEntryCard";
+import NewJournalEntry from "@/components/NewJournalEntry";
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
+import { JournalEntry } from "@prisma/client";
+
+const getJournalEntries = async (): Promise<JournalEntry[]> => {
+  const user = await getUserByClerkId({});
+  const entries = await prisma.journalEntry.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  });
+
+  return entries;
+}
+
+const Journal = async () => {
+  const journalEntries = await getJournalEntries();
+
+  return (
+    <div className="p-10 bg-slate-200/100 h-full">
+      <h1 className="text-3xl mb-8">Journal</h1>
+      <div className="grid grid-cols-3 gap-4">
+        <NewJournalEntry />
+        {journalEntries.map((journalEntry) => (
+          <JournalEntryCard key={journalEntry.id} journalEntry={journalEntry} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Journal;
