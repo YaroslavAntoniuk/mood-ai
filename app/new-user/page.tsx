23fdfefcf2ff4b71e2b9d0d1dab11f5e57941365
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-11 14:55:24 +0200

    Added new user creation logic

---
+++ app/new-user/page.tsx
@@ -0,0 +1,31 @@
+import { prisma } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+const createUser = async () => {
+  const user = await currentUser();
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id as string,
+    },
+  });
+
+  if (!match) {
+    await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      },
+    });
+  }
+
+  redirect('/journal');
+}
+
+const NewUser = async () => {
+  await createUser();
+
+  return <div>Loading...</div>
+}
+
+export default NewUser;
