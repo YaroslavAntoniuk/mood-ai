626a4be4f7d9e71008efe9a7fb13ccad1196139e
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-12 18:22:40 +0200

    Updated journal entry card

--- components/JournalEntryCard.tsx
+++ components/JournalEntryCard.tsx
@@ -1,12 +1,16 @@
 import { JournalEntry } from "@prisma/client";
 
 const JournalEntryCard = ({ journalEntry }: { journalEntry: JournalEntry }) => {
+  const date = new Date(journalEntry.createdAt).toDateString();
+
   return (
-    <div className="card">
-      <h2>{journalEntry.content}</h2>
-      <p>{journalEntry.createdAt.toISOString()}</p>
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg
+    bg-white shadow">
+      <div className="px-4 py-5 sm:px-6">{date}</div>
+      <div className="px-4 py-5 sm:px-6">{journalEntry.content}</div>
+      <div className="px-4 py-5 sm:px-6">{journalEntry.updatedAt.toDateString()}</div>
     </div>
-  );
+  )
 }
 
 export default JournalEntryCard;
