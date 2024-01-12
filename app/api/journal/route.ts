0cc749504fdf93532c0761e41e1d42fba769e592
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-12 18:23:12 +0200

    Added revalidate to the POST function

--- app/api/journal/route.ts
+++ app/api/journal/route.ts
@@ -1,8 +1,9 @@
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
 import { NextResponseWrapper } from '@/utils/response-wrapper'
+import { revalidatePath } from 'next/cache'
 
-export const POST = async (req: Request) => {
+export const POST = async () => {
   const user = await getUserByClerkId()
   const journalEntry = await prisma.journalEntry.create({
     data: {
@@ -10,6 +11,8 @@ export const POST = async (req: Request) => {
       content: 'Hello World!',
     },
   })
+
+  revalidatePath('/journal')
 
   return NextResponseWrapper(journalEntry)
 }
