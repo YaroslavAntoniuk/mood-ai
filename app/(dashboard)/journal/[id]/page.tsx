9e74253d00b62073b1855791aa65796b90fee846
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-12 18:48:57 +0200

    Added journal fetching logic to the journal entry page

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,10 +1,32 @@
+import JournalEntryEditor from "@/components/JournalEntryEditor";
+import { getUserByClerkId } from "@/utils/auth";
+import { prisma } from "@/utils/db";
 import { Params } from "@/utils/types";
+import { JournalEntry } from "@prisma/client";
 
-const JournalEntryPage = ({ params }: Params) => {
+const getJournalEntry = async (id: string): Promise<JournalEntry> => {
+  const user = await getUserByClerkId();
+  const journalEntry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id
+      }
+    },
+  });
+
+  if (!journalEntry) throw new Error("Journal entry not found");
+
+  return journalEntry;
+}
+
+const JournalEntryPage = async ({ params }: { params: Params }) => {
+  const journalEntry = await getJournalEntry(params.id);
+
   return (
-    <div>
+    <div className="w-full h-full">
       <div>Journal Entry Page</div>
-      <div>{params.id}</div>
+      <JournalEntryEditor journalEntry={journalEntry} />
     </div>
   );
 }
