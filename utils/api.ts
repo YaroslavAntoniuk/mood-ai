456cd0bf2a983c400b54253875b76af8326a3cdc
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-14 12:15:34 +0200

    Added update journal entry api function

--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,13 @@
 import { JournalEntry } from '@prisma/client'
-import { ResponseWrapper } from './types'
+import { ErrorUI, ResponseWrapper } from './types'
 
 const createUrl = (path: string) => {
   return `${window.location.origin}${path}`
 }
 
-export const createNewJournalEntry = async (content: string) => {
+export const createNewJournalEntry = async (
+  content: string
+): Promise<JournalEntry | ErrorUI> => {
   const response = await fetch(new Request(createUrl('/api/journal')), {
     method: 'POST',
     headers: {
@@ -15,7 +17,28 @@ export const createNewJournalEntry = async (content: string) => {
   })
 
   if (!response.ok) {
-    throw new Error('Error creating journal entry')
+    return { error: true, message: 'Error creating journal entry' }
+  }
+
+  const data: ResponseWrapper<JournalEntry> = await response.json()
+
+  return data.data
+}
+
+export const updateJournalEntry = async (
+  id: string,
+  content: string
+): Promise<JournalEntry | ErrorUI> => {
+  const response = await fetch(new Request(createUrl(`/api/journal/${id}`)), {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ content }),
+  })
+
+  if (!response.ok) {
+    return { error: true, message: 'Error updating journal entry' }
   }
 
   const data: ResponseWrapper<JournalEntry> = await response.json()
