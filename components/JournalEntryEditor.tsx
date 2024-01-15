595afebc740c6ba7704485bdb24b9596ed2aeabb
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-15 18:40:49 +0200

    Updated entry editor page styling

--- components/JournalEntryEditor.tsx
+++ components/JournalEntryEditor.tsx
@@ -31,7 +31,7 @@ const JournalEntryEditor = ({ journalEntry }: { journalEntry: JournalEntry }) =>
 
 
   return (
-    <div className="w-5/6 h-5/6 p-4">
+    <div className="w-5/6 h-5/6 mt-4">
       <textarea
         className="w-full h-full border p-2 border-gray-200 outline-none text-xl"
         onChange={e => setContent(e.target.value)}
