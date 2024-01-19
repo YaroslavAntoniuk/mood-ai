6a0cf05e04b7575a8740fabf924d31d324c225e6
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-19 14:29:00 +0200

    Updated AI prompt explanation

--- utils/services/ai.service.ts
+++ utils/services/ai.service.ts
@@ -9,27 +9,46 @@ const parser = StructuredOutputParser.fromZodSchema(
     mood: zod
       .string()
       .describe(
-        'The mood of the person who wrote the journal entry(i.e happy, sad, disappointed etc)'
+        `The mood of the person who wrote the journal entry. Should be connected with the color. Examples:
+          passion, love, excitement, anger, danger
+          joy, enthusiasm, creativity, warmth
+          happiness, optimism, hope, energy
+          nature, peace, calmness, growth
+          trust, loyalty, intelligence, sadness
+          luxury, mystery, creativity, wisdom
+          love, romance, femininity, gentleness
+          sophistication, elegance, mystery, death
+          purity, innocence, cleanliness, simplicity
+        `
       )
       .min(1)
-      .default('Unknown'),
+      .default(''),
     subject: zod
       .string()
       .describe('The subject of the journal entry')
       .min(1)
-      .default('Unknown'),
+      .default(''),
     summary: zod
       .string()
       .describe('Quick summary of the entire journal entry')
       .min(1)
-      .default('Unknown'),
+      .default(''),
     color: zod
       .string()
       .describe(
-        'A hexadecimal color representing the mood of the journal entry. Example: #000000 for black'
+        `A hexadecimal color representing the mood of the journal entry, you can usee one or more moods at one time. Example:
+        #FF0000: passion, love, excitement, anger, danger
+        #FFA500: joy, enthusiasm, creativity, warmth
+        #FFFF00: happiness, optimism, hope, energy
+        #00FF00: nature, peace, calmness, growth
+        #0000FF: trust, loyalty, intelligence, sadness
+        #A020F0: luxury, mystery, creativity, wisdom
+        #FFC0CB: love, romance, femininity, gentleness
+        #000000: sophistication, elegance, mystery, death
+        #FFFFFF: purity, innocence, cleanliness, simplicity`
       )
       .min(1)
-      .default('Unknown'),
+      .default('#ffffff'),
     negative: zod
       .boolean()
       .describe(
@@ -43,7 +62,7 @@ const getPrompt = async (content: string) => {
   const format_instructions = parser.getFormatInstructions()
   const prompt = new PromptTemplate({
     template:
-      'Analyze the following journal entry. if you cannot analyze any of the values of the object just leave them blank, like empty string: "" for string type, and false for booleans, Follow the intrusctions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',
+      'Analyze the following journal entry. All fields are required. Do not leave them blank. If you cannot define the value use neutral tone. Follow the instructions and format your response to match the format instructions, no matter what! \n{format_instructions}\n{entry}',
     inputVariables: ['entry'],
     partialVariables: { format_instructions },
   })
