8085adaecd3306daa47c926f3de0dbedd630ea63
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-24 15:50:14 +0200

    Added new AI function for asking a questions

--- utils/services/ai.service.ts
+++ utils/services/ai.service.ts
@@ -1,8 +1,12 @@
 import { PromptTemplate } from '@langchain/core/prompts'
 import { ChatOpenAI } from '@langchain/openai'
+import { loadQARefineChain } from 'langchain/chains'
+import { Document } from 'langchain/document'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 import { StructuredOutputParser } from 'langchain/output_parsers'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 import zod from 'zod'
-import { AnalysisDTO } from '../types'
+import { AnalysisDTO, JournalQAEntry } from '../types'
 
 const parser = StructuredOutputParser.fromZodSchema(
   zod.object({
@@ -91,4 +95,28 @@ export const analyze = async (content: string): Promise<AnalysisDTO> => {
   } catch (error) {
     throw Error(`Failed to parse response: ${error}`)
   }
+}
+
+export const qa = async (question: string, entries: JournalQAEntry[]) => {
+  const docs = entries.map(
+    (entry) =>
+      new Document({
+        pageContent: entry.content,
+        metadata: { id: entry.id, createdAt: entry.createdAt },
+      })
+  )
+
+  const model = new ChatOpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+
+  const response = await chain.invoke({
+    input_documents: relevantDocs,
+    question,
+    prompt: 'Talk like a mental assistant',
+  })
+
+  return response.output_text
 }
