f59915d2557ec17407dd4a2ee0df02f8344a91bd
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-24 16:12:13 +0200

    Updated Journal Entry card styles

--- components/JournalEntryCard.tsx
+++ components/JournalEntryCard.tsx
@@ -5,7 +5,7 @@ const JournalEntryCard = ({ journalEntry }: { journalEntry: FullJournalEntry }) 
 
   return (
     <div className="divide-y divide-gray-200 rounded-lg
-    bg-white shadow min-w-[100px]">
+    bg-white shadow min-w-[100px] h-[250px]">
       <div className="px-4 py-5 sm:px-6 flex justify-between">
         <div>{date}</div>
         <div className="flex flex-row gap-4 items-center">
@@ -13,7 +13,9 @@ const JournalEntryCard = ({ journalEntry }: { journalEntry: FullJournalEntry }) 
           <div className="w-8 h-8 rounded-3xl" style={{ backgroundColor: journalEntry.analysis?.color || 'gray' }}></div>
         </div>
       </div>
-      <div className="px-4 py-5 sm:px-6 text-ellipsis">{journalEntry.content}</div>
+      <div className="px-4 py-5 sm:px-6">
+        <p className=" line-clamp-3 leading-7 min-h-20">{journalEntry.content}</p>
+      </div>
       <div className="px-4 py-5 sm:px-6">{journalEntry.updatedAt.toDateString()}</div>
     </div>
   )
