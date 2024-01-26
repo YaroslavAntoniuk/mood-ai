e539f9fdc36d57d44df1dd7d87ef6ca80441c0f9
YaroslavAntoniuk <81804109+YaroslavAntoniuk@users.noreply.github.com>
2024-01-26 14:08:05 +0200

    Updated journal entry page styling

--- components/JournalEntryEditor.tsx
+++ components/JournalEntryEditor.tsx
@@ -52,8 +52,8 @@ const JournalEntryEditor = ({ journalEntry }: { journalEntry: FullJournalEntry }
 
 
   return (
-    <div className="w-full h-full mt-4 grid grid-cols-[2fr_1fr] gap-4">
-      <div className="cols-span-1">
+    <div className="w-full h-full mt-4 grid grid-rows-2 lg:grid-cols-[2fr_1fr] gap-4">
+      <div>
         {isLoading && <div className="absolute top-0 left-0 w-full h-full bg-white/50 flex justify-center items-center">
           <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
         </div>}
