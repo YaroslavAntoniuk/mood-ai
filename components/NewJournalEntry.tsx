1d4d93054fa2b3663b3088d16cd75e2daf6da972
YaroslavAntoniuk <yaroslav.antoniuk.dev@gmail.com>
2025-05-19 16:34:57 +0300

    Added loading indicator for new entry card

--- components/NewJournalEntry.tsx
+++ components/NewJournalEntry.tsx
@@ -4,27 +4,41 @@ import { createNewJournalEntry } from "@/utils/api";
 import { useRouter } from "next/navigation";
 import { isErrorUI } from "@/utils/types";
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 const NewJournalEntry = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleAddJournalEntry = async () => {
+    setIsLoading(true);
+    try {
+      const data = await createNewJournalEntry('');
 
-    const data = await createNewJournalEntry('');
+      if (isErrorUI(data)) {
+        toast.error(data.message);
+        return;
+      }
 
-    if (isErrorUI(data)) {
-      'use server'
-      toast.error(data.message);
-      return;
+      router.push(`/journal/${data.id}`);
+      router.refresh();
+    } catch (error) {
+      toast.error('Failed to create journal entry');
+    } finally {
+      setIsLoading(false);
     }
-
-    router.push(`/journal/${data.id}`)
   }
 
   return (
-    <div className="cursor-pointer bg-white shadow-md rounded-md px-6 py-4">
+    <div className="cursor-pointer bg-white shadow-md rounded-md px-6 py-4 relative">
       <div className="px-4 py-5 sm:p-6" onClick={handleAddJournalEntry}>
         <span className="text-xl">New Journal Entry</span>
       </div>
+      {isLoading && (
+        <div className="absolute top-0 left-0 w-full h-full bg-white/50 flex justify-center items-center">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      )}
     </div>
   );
 }
